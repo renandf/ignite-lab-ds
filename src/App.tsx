4a0dd8b261ad3fb14f521cb31cbfee364201d46b
Renan Castro <renandf@gmail.com>
2022-10-12 20:25:31 +1100

    feat: :tada: Initial commit with basic structure using Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
