6bd118b1befc1e2ef6da8393c7ed19db3f83b6a0
Renan Castro <renandf@gmail.com>
2022-10-13 16:41:53 +1100

    feat: :lipstick: Create design tokens for colors and font sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,24 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
+    colors: {
+      'gray-100': '#e1e1e6',
+      'gray-200': '#c4c4cc',
+      'gray-400': '#7c7c8a',
+      'gray-800': '#202024',
+      'gray-900': '#121214',
+
+      'cyan-300': '#9be1fb',
+      'cyan-500': '#81d8f7',
+    },
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
