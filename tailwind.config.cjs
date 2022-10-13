9fe91aa0ee57b9bc0f1229a30588b79ad99d6612
Renan Castro <renandf@gmail.com>
2022-10-13 17:42:51 +1100

    feat: Create Heading component
    Heading component follows the same structure as the Text component, but using heading sizes. To be used with H1, H2, H3...

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,11 +15,11 @@ module.exports = {
       'cyan-500': '#81d8f7',
     },
     fontSize: {
-      xs: 14,
-      sm: 16,
-      md: 18,
-      lg: 20,
-      xl: 24,
+      'xs': 14,
+      'sm': 16,
+      'md': 18,
+      'lg': 20,
+      'xl': 24,
       '2xl': 32,
     },
     extend: {
