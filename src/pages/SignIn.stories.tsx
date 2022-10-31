d1d9fcf6323695df194f21caee5a3b743737798c
Renan Castro <renandf@gmail.com>
2022-10-31 15:12:11 +1100

    feat: Add tests to Storybook

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+import { SignIn } from './SignIn'
+
+export default {
+  title: 'Pages/Sign in',
+  component: SignIn,
+  args: {},
+  argTypes: {}
+} as Meta
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    userEvent.type(canvas.getByPlaceholderText('e.g., jane@email.com'), 'email@emailtest.com')
+    userEvent.type(canvas.getByPlaceholderText('***********'), 'P4ssw@rd123!')
+
+    userEvent.click(canvas.getByRole('button'))
+
+    await waitFor(() => {
+      expect(canvas.getByText('Login successful!')).toBeInTheDocument()
+    })
+  }
+}
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,82 @@
+import { FormEvent, useState } from 'react';
+import { Logo } from '../components/Logo';
+import { Heading } from '../components/Heading';
+import { Text } from '../components/Text';
+import { TextInput } from '../components/TextInput';
+import { Envelope, Lock } from 'phosphor-react';
+import { Checkbox } from '../components/Checkbox';
+import { Button } from '../components/Button';
+
+export function SignIn() {
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false)
+
+  function handleSignIn(e: FormEvent) {
+    e.preventDefault()
+
+    setIsUserSignedIn(true)
+  }
+
+  return (
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
+
+      <header className='flex flex-col items-center'>
+        <Logo width={106} height={120} />
+
+        <Heading size="lg" className='mt-4'>
+          Ignite Lab
+        </Heading>
+
+        <Text size="lg" className='text-gray-400 mt-1'>
+          Log in and start using it!
+        </Text>
+      </header>
+
+      <form onSubmit={handleSignIn} className='flex flex-col items-stretch w-full max-w-sm mt-10 gap-4'>
+        {isUserSignedIn && <Text>Login successful!</Text>}
+
+        <label htmlFor="email" className='flex flex-col gap-3'>
+          <Text className='font-semibold'>Email</Text>
+
+          <TextInput.Root>
+            <TextInput.Icon><Envelope /></TextInput.Icon>
+
+            <TextInput.Input type='email' placeholder='e.g., jane@email.com' />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className='flex flex-col gap-3'>
+          <Text className='font-semibold'>Password</Text>
+
+          <TextInput.Root>
+            <TextInput.Icon><Lock /></TextInput.Icon>
+
+            <TextInput.Input type='password' placeholder='***********' />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember" className='flex items-center gap-2'>
+          <Checkbox id='remember' />
+          <Text size='sm' className='text-gray-200'>
+            Remember me for 30 days
+          </Text>
+        </label>
+
+        <Button type='submit' className='mt-4'>
+          Enter the platform
+        </Button>
+
+      </form>
+
+      <footer className='flex flex-col items-center gap-4 mt-8'>
+        <Text asChild size='sm'>
+          <a href="" className='text-gray-400 underline hover:text-gray-200'>Forgot your password?</a>
+        </Text>
+        <Text asChild size='sm'>
+          <a href="" className='text-gray-400 underline hover:text-gray-200'>No account yet? Sign up!</a>
+        </Text>
+      </footer>
+
+    </div>
+  )
+}
+
